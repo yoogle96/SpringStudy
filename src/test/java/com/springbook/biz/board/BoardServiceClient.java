41f5e310eb967c603c2e40d7fa246727594f63d6
yoogle96 <dmltjs851@gmail.com>
2020-01-03 01:00:06 +0900

    Complete CLASS 06 비즈니스 컴포넌트 실습1

---
+++ src/test/java/com/springbook/biz/board/BoardServiceClient.java
@@ -0,0 +1,28 @@
+package com.springbook.biz.board;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.util.List;
+
+public class BoardServiceClient {
+
+    public static void main(String[] args){
+        AbstractApplicationContext container = new GenericXmlApplicationContext("applicationContext.xml");
+
+        BoardService boardService = (BoardService)container.getBean("boardService");
+
+        BoardVO vo = new BoardVO();
+        vo.setTitle("임시 제목");
+        vo.setWriter("홍길동");
+        vo.setContent("임시 내용...........");
+        boardService.insertBoard(vo);
+
+        List<BoardVO> boardVOList = boardService.getBoardList(vo);
+        for(BoardVO board : boardVOList){
+            System.out.println("--->" + board.toString());
+        }
+
+        container.close();
+    }
+}
