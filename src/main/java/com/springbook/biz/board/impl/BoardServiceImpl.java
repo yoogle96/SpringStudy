5256886a5d40c842a6cb14958bb74695284e9069
yoogle96 <dmltjs851@gmail.com>
2020-01-07 05:40:46 +0900

    Complete CLASS 07 트랜잭션 처리

--- src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -18,6 +18,7 @@ public class BoardServiceImpl implements BoardService {
 //            throw new IllegalArgumentException("0번 글은 등록할 수 없습니다.");
 //        }
         boardDAO.insertBoard(vo);
+        boardDAO.insertBoard(vo);
     }
 
     @Override
--- src/test/java/com/springbook/biz/board/BoardServiceClient.java
+++ src/test/java/com/springbook/biz/board/BoardServiceClient.java
@@ -13,10 +13,11 @@ public class BoardServiceClient {
         BoardService boardService = (BoardService)container.getBean("boardService");
 
         BoardVO vo = new BoardVO();
+        vo.setSeq(100);
         vo.setTitle("임시 제목");
         vo.setWriter("홍길동");
         vo.setContent("임시 내용...........");
-        boardService.insertBoard(vo);
+//        boardService.insertBoard(vo);
 
         List<BoardVO> boardVOList = boardService.getBoardList();
         for(BoardVO board : boardVOList){
